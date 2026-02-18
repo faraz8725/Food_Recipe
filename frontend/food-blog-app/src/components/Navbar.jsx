008f3b2f18310666824920d34d044e7a3b8762d6
Faraz Sarfaraz Agha <fagha4697@gmail.com>
2026-02-18 22:39:30 +0530

    Navbar Updates

--- frontend/food-blog-app/src/components/Navbar.jsx
+++ frontend/food-blog-app/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React,{useState}from 'react'
 
 import Modal from "./Modal";
 export default function Navbar(){
-  const [isOpen,setIsOpen]=useState(false);
+  const [isOpen,setIsOpen]=useState(false); //use of useState
   const checkLogin=()=>{
     setIsOpen(true);
   }
@@ -15,7 +15,7 @@ export default function Navbar(){
       <li>My Recipe</li>
       <li>Favrouites</li>
       <li onClick={checkLogin}>Login</li>
-      <li>Logout</li>
+      
           </ul>
           </header>
             { (isOpen)&& <Modal onClose={()=>setIsOpen(false)}/>}
